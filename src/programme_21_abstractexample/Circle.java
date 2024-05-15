8a16844506234f69de21bd0b5736b2e64f9583d1
shubloha <sampanchal1994@gmail.com>
2024-05-15 17:37:20 +0000

    java-week-4-writing-hw-shubham_lohar-

---
+++ src/programme_21_abstractexample/Circle.java
@@ -0,0 +1,12 @@
+package programme_21_abstractexample;
+
+import java.awt.*;
+
+/**
+ * 3. Create a Third class name Circle1 which extends by Shape.
+ */
+public class Circle extends Shape {
+    public void draw() {
+        System.out.println("drawing circle");
+    }
+}
